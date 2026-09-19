bf314d57c14af1321a8874f692db81991f5d725b
Gutz1913 <andresfgutierrezv@gmail.com>
2026-09-19 14:56:36 -0500

    Add new code for age calculation.

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,12 +1,61 @@
-﻿namespace HelloWorld;
+﻿using Shared;
+
+namespace HelloWorld;
 
 class Program
 {
     static void Main()
     {
-        var Name = "Andrés";
-        Name = "Andrés Gutiérrez";
-        Console.WriteLine($"Hello World! My name is {Name}.");
-        Console.WriteLine();
+        var answer = string.Empty;
+        var options = new List<string> { "s", "n" };
+
+        do
+        {
+            try
+            {
+                DateOnly dateConverted = new DateOnly();
+                Console.WriteLine("¡Hola, Bienvenido al calculador de años!");
+                var nameInput = ConsoleExtension.GetString("Escribe tu nombre: ");
+                Console.WriteLine($"Un gusto conocerte {nameInput}");
+                var birthDateInput = ConsoleExtension.GetString("Escribe tu fecha de nacimiento en formato dd/mm/yy: ");
+                bool isDateValid = DateOnly.TryParse(birthDateInput, out dateConverted);
+                if (!isDateValid)
+                {
+
+
+                    throw new Exception($"La fecha de nacimiento no es válida, usted nos envió este dato erróneo: {birthDateInput}");
+                }
+
+                var person = new Person()
+                {
+                    Name = nameInput,
+                    BirthDate = dateConverted,
+                    Age = DateTime.Now.Year - dateConverted.Year
+                };
+
+                Console.WriteLine($"Tú nombre es.............: {person.Name}");
+                Console.WriteLine($"Tú fecha de nacimiento es: {person.BirthDate}");
+                Console.WriteLine($"Tú edad es...............: {person.Age}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+
+            do
+            {
+                answer = ConsoleExtension.GetValidOptions("¿Deseas continuar? ¿[S]í, [N]o?: ", options);
+            }
+            while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+        }
+        while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+        Console.WriteLine(":::::: GAME OVER ::::::");
     }
+}
+
+public class Person
+{
+    public string Name { get; set; } = string.Empty;
+    public int Age { get; set; }
+    public DateOnly BirthDate { get; set; }
 }
